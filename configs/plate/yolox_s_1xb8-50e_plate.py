32e0fec73bc31ed5e89bb971d0c42329f3b76169
root <root@Mon-s-Daddy-PC>
2022-11-08 08:38:35 +0700

    update config

--- configs/plate/yolox_s_1xb8-50e_plate.py
+++ configs/plate/yolox_s_1xb8-50e_plate.py
@@ -169,7 +169,7 @@ test_evaluator = val_evaluator
 # training settings
 max_epochs = 50
 num_last_epochs = 15
-interval = 5
+interval = 3
 
 train_cfg = dict(max_epochs=max_epochs, val_interval=interval)
 
